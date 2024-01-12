b8d4c7cbca7dc1c63f5db85489a8f4280e362bb1
Abdullah Islam <155878180+abdullahislam123@users.noreply.github.com>
2024-01-12 22:56:13 +0500

    Add files via upload
    calculator program using function

---
+++ calcuatorfunction.cpp
@@ -0,0 +1,23 @@
+#include<iostream>
+using namespace std;
+int sum(int n1,int n2)
+{
+    int x;
+    x = n1+n2;
+    return x;
+}
+int main ()
+{
+    int n1,n2;
+    char op;
+    cout<<"Enter first number : ";
+    cin>>n1;
+    cout<<"Enter operator (+,-,*,/)";
+    cin>>op;
+    cout<<"Enter second number : ";
+    cin>>n2;
+    if (op=='+')
+    {
+
+    }
+}
